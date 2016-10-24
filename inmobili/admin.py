7c663269e156bf66c95c7c1add8947fef8c762fa
rodrilopez <rodrilopezypl@gmail.com>
2016-10-24 08:28:38 -0300

    Delete admin.py

--- inmobili/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from inmobili.models import *
-# Register your models here.
-admin.site.register(Casa)
-admin.site.register(Perfil)
-admin.site.register(Comment)
-admin.site.register(Image)
-admin.site.register(Fav)
